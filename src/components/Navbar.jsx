3dc6774300a10614cd2bf111e312cc78808135f0
hannanxo <ah4hannan@gmail.com>
2023-02-14 12:34:47 +0500

    Added Navbar and Homepage components.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Logo from "../assets/logo1.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Socials from "./Socials";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => {
+    setNav(!nav);
+    console.log(nav);
+  };
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "50px" }} />
+      </div>
+
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* Socials */}
+      <Socials />
+    </div>
+  );
+};
+
+export default Navbar;
