53ee5d88da0683975197f355973f4028e7d47efb
hannanxo <ah4hannan@gmail.com>
2023-02-20 21:04:45 +0500

    Middleware setup.

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,34 +1,37 @@
+const asyncHandler = require("express-async-handler");
+// To avoid using try and catch
+
 // @desc Get blogs
 // @route  GET /api/blogs
 // @access Private
-const getBlogs = (req, res) => {
+const getBlogs = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get blogs" });
-};
+});
 
 // @desc Set blogs
 // @route  POST /api/blogs
 // @access Private
-const setBlog = (req, res) => {
+const setBlog = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Set blogs" });
-};
+});
 
 // @desc Update blogs
 // @route  PUT /api/blogs/:id
 // @access Private
-const updateBlog = (req, res) => {
+const updateBlog = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update blog ${req.params.id}` });
-};
+});
 
 // @desc Delete blogs
 // @route  DELETE /api/blogs/:id
 // @access Private
-const deleteBlog = (req, res) => {
+const deleteBlog = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete blog ${req.params.id}` });
-};
+});
 
 module.exports = {
   getBlogs,
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // entry point to our server
 const express = require("express");
 const dotenv = require("dotenv").config(); //for env variables
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 // Initial express
@@ -11,4 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/blogs", require("./routes/blogRoutes"));
+
+app.use(errorHandler); //will override express's default error handler (HTML)
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
